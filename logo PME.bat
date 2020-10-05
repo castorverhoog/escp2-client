0c15e9e8faecf04ff0075da4b9a044ae8f5a5460
Castor Verhoog <c.a.verhoog@student.tudelft.nl>
2020-10-05 11:57:44 +0200

    Initial commit

---
+++ logo PME.bat
@@ -0,0 +1,4 @@
+ECHO "is it working"
+cd "C:\Users\bobbr\Downloads\Software ESCP2-20190808T075132Z-001\Software ESCP2\GUI\EPNCS-tweak\output"
+copy logo_PME-MNE.prn /B "\\DESKTOP-E557MR2\EPSON XP-235 Series"
+PAUSE
---
+++ output/raster.bat
@@ -0,0 +1,4 @@
+ECHO "is it working"
+cd "C:\Users\bobbr\Downloads\Software ESCP2-20190808T075132Z-001\Software ESCP2\GUI\EPNCS-tweak\output"
+copy nxm_raster.prn /B "\\DESKTOP-E557MR2\EPSON XP-235 Series"
+PAUSE
